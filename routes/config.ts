eb7df8afb2e8988bc7c2737eca87dcf9f764abf5
jjungeun <jungeun9729@gmail.com>
2020-07-31 23:54:39 +0900

    dashboard

--- routes/config.ts
+++ routes/config.ts
@@ -3,5 +3,6 @@ import * as k8s from "@kubernetes/client-node";
 const kc = new k8s.KubeConfig();
 kc.loadFromDefault();
 export const k8sCoreV1Api = kc.makeApiClient(k8s.CoreV1Api);
+export const k8sAppsV1Api = kc.makeApiClient(k8s.AppsV1Api);
 
 
--- routes/index.ts
+++ routes/index.ts
@@ -1,13 +1,33 @@
 import { Router, NextFunction, Request, Response } from "express";
-import { k8sCoreV1Api } from "./config";
+import { k8sAppsV1Api } from "./config";
 import { wrapper } from "./wrap";
 
 const router = Router();
+const NLB_URL = "http://kuberkuber-cluster-bace65abd86cb82e.elb.ap-northeast-2.amazonaws.com"
 
 /* GET Dashboard. */
 router.get('/', wrapper(async (req : Request, res : Response, next : NextFunction)  => {
-  const kuberRes = await k8sCoreV1Api.listNamespacedPod('test');
-  res.status(200).json(kuberRes);
+  const namespace = req.query['namespace'];
+  if (namespace != undefined)
+  {
+    const kuberRes = await k8sAppsV1Api.listNamespacedDeployment('test');
+    let deploys: Array<{}> = [];
+    for (const item of kuberRes.body['items']) {
+      let itemDict: {[key: string]: string} = {};
+      const metadata = item.metadata;
+      const status = item.status;
+      const container = item.spec?.template.spec?.containers[0]
+      itemDict['image'] = container?.image!;
+      itemDict['port'] = container?.ports![0].containerPort.toString() || '';
+      itemDict['name'] = metadata?.name!;
+      itemDict['status'] = status?.conditions![0].status || '';
+      itemDict['endpoint'] = NLB_URL + '/' + namespace + '/' + itemDict['name'];
+      deploys.push(itemDict);
+    }
+    res.status(200).send(JSON.stringify(deploys));
+  } else {
+    res.status(400).send('Bad Request : namespace should be specify');
+  }
 }));
 
 module.exports = router;
